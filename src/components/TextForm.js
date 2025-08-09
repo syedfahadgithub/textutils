dcf24c283ca0200f210d33499443571041cb6a12
Syed Fahad <fahadshahf31@gmail.com>
2025-08-09 18:34:09 +0500

    5th commit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -12,6 +12,16 @@ const TextForm = (props) => {
     let newtext = text.toLowerCase()
     setText(newtext)
   }
+  //clear the text box
+  const handleclear = ()=>{
+    let newtext = ""
+    setText(newtext)
+  }
+  //remove extra spaces
+  const handlespaces = ()=>{
+    let newtext = text.split(/[ ]+/);
+    setText(newtext.join(" "))
+  }
   //on each chng event perform
   const chnghandle = (event)=>{
     setText(event.target.value)
@@ -26,6 +36,8 @@ const TextForm = (props) => {
       </div>
       <button onClick={handleUp} className="btn btn-outline-primary mx-3">Convert To Upper Case</button>
       <button onClick={handleLow} className="btn btn-outline-primary">Convert To Lower Case</button>
+      <button onClick={handleclear} className="btn btn-outline-primary mx-3">Clear Text</button>
+      <button onClick={handlespaces} className="btn btn-outline-primary mx-3">Remove Extra Spaces</button>
     </div>
     <div className="container">
         <h2 className="my-3">Your Text Summary</h2>
